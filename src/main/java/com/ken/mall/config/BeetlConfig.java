d797dedd98a6044779eeb71cf2b25f77e91cf723
ligs <236813207@qq.com>
2019-04-24 19:12:19 +0800

    集成beetl

--- src/main/java/com/ken/mall/config/BeetlConfig.java
+++ src/main/java/com/ken/mall/config/BeetlConfig.java
@@ -22,17 +22,6 @@ public class BeetlConfig {
 
     @Bean(initMethod = "init", name = "beetlConf")
     public BeetlGroupUtilConfiguration getBeetlGroupUtilConfiguration() {
-        /*BeetlGroupUtilConfiguration beetlGroupUtilConfiguration = new BeetlGroupUtilConfiguration();
-        ResourcePatternResolver patternResolver = ResourcePatternUtils.getResourcePatternResolver(new DefaultResourceLoader());
-        try {
-            // WebAppResourceLoader 配置root路径是关键
-            WebAppResourceLoader webAppResourceLoader =
-                    new WebAppResourceLoader(patternResolver.getResource("classpath:/").getFile().getPath());
-            beetlGroupUtilConfiguration.setResourceLoader(webAppResourceLoader);
-            beetlGroupUtilConfiguration.init();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
         //读取配置文件信息
         BeetlGroupUtilConfiguration beetlGroupUtilConfiguration = new BeetlGroupUtilConfiguration();
         //获取Spring Boot 的ClassLoader
@@ -43,8 +32,6 @@ public class BeetlConfig {
         //beetlGroupUtilConfiguration.setConfigProperties(extProperties);//额外的配置，可以覆盖默认配置，一般不需要
         ClasspathResourceLoader cploder = new ClasspathResourceLoader(loader, "WEB-INF/views/");
         beetlGroupUtilConfiguration.setResourceLoader(cploder);
-        //ResourcePatternResolver patternResolver = ResourcePatternUtils.getResourcePatternResolver(new DefaultResourceLoader());
-        //beetlGroupUtilConfiguration.setConfigFileResource(patternResolver.getResource("classpath:beetl.properties"));
         beetlGroupUtilConfiguration.init();
         //如果使用了优化编译器，涉及到字节码操作，需要添加ClassLoader
         beetlGroupUtilConfiguration.getGroupTemplate().setClassLoader(loader);
@@ -56,7 +43,6 @@ public class BeetlConfig {
     public BeetlSpringViewResolver getBeetlSpringViewResolver(
             @Qualifier("beetlConf") BeetlGroupUtilConfiguration beetlGroupUtilConfiguration) {
         BeetlSpringViewResolver beetlSpringViewResolver = new BeetlSpringViewResolver();
-        //beetlSpringViewResolver.setPrefix("WEB-INF/views/");
         beetlSpringViewResolver.setSuffix(".html");
         beetlSpringViewResolver.setContentType("text/html;charset=UTF-8");
         beetlSpringViewResolver.setOrder(0);
